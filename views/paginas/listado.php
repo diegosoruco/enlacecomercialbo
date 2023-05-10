46c16f2c8b97d23b15a138338b3aee3180bd5be5
diegosoruco <diegosoruco@live.com>
2023-05-10 15:30:17 -0400

    cambios en el index

--- views/paginas/listado.php
+++ views/paginas/listado.php
@@ -7,7 +7,7 @@
 
             <div class="contenido-anuncio">
                 <h3><?php echo $propiedad->titulo; ?></h3>
-                <p><?php echo $propiedad->descripcion; ?></p>
+                <p><?php echo substr($propiedad->descripcion, 0, 200); ?>...</p>
                 <p class="precio">$<?php echo $propiedad->precio; ?></p>
 
                 <ul class="iconos-caracteristicas">
