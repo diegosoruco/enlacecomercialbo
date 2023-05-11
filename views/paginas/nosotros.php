4234d30a529ce2c2795f81d43633b8d7eccd25a5
diegosoruco <diegosoruco@live.com>
2023-05-10 20:45:04 -0400

    pagina nosotros

--- views/paginas/nosotros.php
+++ views/paginas/nosotros.php
@@ -12,22 +12,19 @@
 
         <div class="texto-nosotros">
             <blockquote>
-                25 Años de experiencia
+                15 Años de experiencia nos respalda
             </blockquote>
 
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque at facilisis orci. In tempor
-                pharetra eros, id porttitor tellus rhoncus in. Curabitur aliquet sodales felis, sit amet aliquam
-                justo aliquam at. Nunc vel suscipit urna. Aliquam pellentesque volutpat arcu, id luctus ipsum
-                posuere vitae. Pellentesque ante tellus, elementum nec tincidunt at, porta ut enim. Mauris pulvinar
-                lectus vitae leo volutpat pharetra. Sed ornare magna ipsum, vitae finibus nisi semper at. Nam eget
-                justo sed ligula viverra volutpat.
-                Donec dolor neque, vehicula sed orci vitae, rutrum blandit metus. Integer tincidunt tellus in nisi.
+            <p>Bienvenidos a nuestra empresa inmobiliaria con 15 años de experiencia. Somos un equipo de profesionales apasionados por el sector de bienes raíces, que hemos acumulado un conocimiento sólido del mercado inmobiliario y de las necesidades y expectativas de nuestros clientes.
+
+                Durante nuestros 15 años de experiencia, hemos logrado desarrollar una amplia red de contactos y relaciones en el sector inmobiliario, lo que nos permite tener acceso a información valiosa sobre las últimas tendencias del mercado y oportunidades de inversión.
+
+                En nuestra empresa, nos esforzamos por ofrecer un servicio de alta calidad y personalizado, enfocado en satisfacer las necesidades de nuestros clientes. Nos tomamos el tiempo de conocer a cada uno de ellos, evaluar sus necesidades y ayudarles a encontrar la propiedad ideal, ya sea para comprar, vender o alquilar.
             </p>
             <p>
-                Donec quam enim, congue quis dolor sed, scelerisque finibus felis. Sed sit amet rhoncus sapien.
-                Etiam vel porttitor justo. Donec iaculis dictum porttitor. Vestibulum aliquet lacinia ligula non
-                fringilla. Suspendisse non semper nulla. Fusce ac ipsum erat. Nulla faucibus, magna eget volutpat
-                rutrum, est ipsum tincidunt erat, id egestas nunc lectus sed neque. Ut ut turpis sem. Nulla.
+                Nuestro equipo de profesionales altamente capacitados, cuenta con habilidades clave como la capacidad de evaluación de mercado, marketing, negociación y atención al cliente, lo que nos permite garantizar una transacción exitosa y una experiencia satisfactoria para nuestros clientes.
+
+                Nos enorgullece haber logrado establecer una reputación sólida en el mercado inmobiliario, basada en la integridad, la transparencia y el compromiso con nuestros clientes. Esperamos tener la oportunidad de trabajar con usted y ayudarle a cumplir sus objetivos de bienes raíces.
             </p>
         </div>
     </div>
